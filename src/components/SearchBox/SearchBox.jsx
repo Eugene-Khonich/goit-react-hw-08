58abd3e7025b82d6b23035ee895c8118c4620a39
Eugene-Khonich <ehonich@gmail.com>
2024-11-12 00:04:45 +0200

    done

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,7 @@
 import css from './SearchBox.module.css';
-import { changeFilter } from '../../redux/filtersSlice';
+import { changeFilter } from '../../redux/filters/slice';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectNameFilter } from '../../redux/selectors';
+import { selectNameFilter } from '../../redux/filters/selectors';
 
 const SearchBox = () => {
   const dispatch = useDispatch();
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import ContactForm from '../../components/ContactForm/ContactForm';
 import ContactList from '../../components/ContactList/ContactList';
+import SearchBox from '../../components/SearchBox/SearchBox';
 import { selectLoading } from '../../redux/contacts/selectors';
 import { useEffect } from 'react';
 import { fetchContacts } from '../../redux/contacts/operations';
@@ -20,6 +21,7 @@ const ContactsPage = () => {
   return (
     <div style={style}>
       <ContactForm />
+      <SearchBox />
       <div>{isLoading && 'Request in progress...'}</div>
       <ContactList />
     </div>
